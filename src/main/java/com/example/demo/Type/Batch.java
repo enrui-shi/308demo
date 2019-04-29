6aa205d8fa20fcc8afee28e73010a6f721722fec
ruiqi <ruiqi@xiamu.local>
2019-04-29 17:14:37 -0400

    create batch object

---
+++ src/main/java/com/example/demo/Type/Batch.java
@@ -0,0 +1,91 @@
+package com.example.demo.Type;
+
+import javax.persistence.OneToOne;
+
+public class Batch {
+
+    private int numBatch;
+
+    private String stateName;
+
+    @OneToOne
+    private Bound numOfMMBound;
+
+    @OneToOne
+    private Bound equalPopulationBound;
+
+    @OneToOne
+    private Bound compactnessBound;
+
+    @OneToOne
+    private Bound partisanFairnessBound;
+
+    @OneToOne
+    private Bound natureConstrainBound;
+
+    public Batch(int numBatch, String stateName, Bound numOfMMBound, Bound equalPopulationBound, Bound compactnessBound, Bound partisanFairnessBound, Bound natureConstrainBound) {
+        this.numBatch = numBatch;
+        this.stateName = stateName;
+        this.numOfMMBound = numOfMMBound;
+        this.equalPopulationBound = equalPopulationBound;
+        this.compactnessBound = compactnessBound;
+        this.partisanFairnessBound = partisanFairnessBound;
+        this.natureConstrainBound = natureConstrainBound;
+    }
+
+    public int getNumBatch() {
+        return numBatch;
+    }
+
+    public String getStateName() {
+        return stateName;
+    }
+
+    public Bound getNumOfMMBound() {
+        return numOfMMBound;
+    }
+
+    public Bound getEqualPopulationBound() {
+        return equalPopulationBound;
+    }
+
+    public Bound getCompactnessBound() {
+        return compactnessBound;
+    }
+
+    public Bound getPartisanFairnessBound() {
+        return partisanFairnessBound;
+    }
+
+    public Bound getNatureConstrainBound() {
+        return natureConstrainBound;
+    }
+
+    public void setNumBatch(int numBatch) {
+        this.numBatch = numBatch;
+    }
+
+    public void setStateName(String stateName) {
+        this.stateName = stateName;
+    }
+
+    public void setNumOfMMBound(Bound numOfMMBound) {
+        this.numOfMMBound = numOfMMBound;
+    }
+
+    public void setEqualPopulationBound(Bound equalPopulationBound) {
+        this.equalPopulationBound = equalPopulationBound;
+    }
+
+    public void setCompactnessBound(Bound compactnessBound) {
+        this.compactnessBound = compactnessBound;
+    }
+
+    public void setPartisanFairnessBound(Bound partisanFairnessBound) {
+        this.partisanFairnessBound = partisanFairnessBound;
+    }
+
+    public void setNatureConstrainBound(Bound natureConstrainBound) {
+        this.natureConstrainBound = natureConstrainBound;
+    }
+}
--- src/main/resources/static/script/batch.js
+++ src/main/resources/static/script/batch.js
@@ -3,11 +3,15 @@ $(document).ready(function(){
     var batch_form = $("#batch");
     batch_form.submit( function(e) {
         var preferenceBound = {}
-        var batch_data = {stateName: $("input[name='state']:checked").val(), numBatch: $('#num-of-batch').val() ,
-            mmMIN: $("input[name='mmMIN']").val(), mmMAX: $("input[name='mmMAX']").val(), eqMIN: $("input[name='eqMIN']").val(),
-            eqMAX: $("input[name='eqMAX']").val(), cMIN: $("input[name='cMIN']").val(), cMAX: $("input[name='cMAX']").val(),
-            pfMIN: $("input[name='pfMIN']").val(), pfMAX: $("input[name='pfMAX']").val(),ncMIN: $("input[name='ncMIN']").val(),
-            ncMAX: $("input[name='ncMAX']").val()};
+        var batch_data = {};
+        batch_data[stateName] = $("input[name='state']:checked").val();
+        batch_data[numBatch] = $('#num-of-batch').val();
+        batch_data[numOfMMBound] = { mmMIN: $("input[name='mmMIN']").val(), mmMAX: $("input[name='mmMAX']").val()};
+        batch_data[equalPopulationBound] = { eqMIN: $("input[name='eqMIN']").val(), eqMAX: $("input[name='eqMAX']").val()};
+        batch_data[compactnessBound] = { cMIN: $("input[name='cMIN']").val(), cMAX: $("input[name='cMAX']").val()};
+        batch_data[partisanFairnessBound] = { pfMIN: $("input[name='pfMIN']").val(), pfMAX: $("input[name='pfMAX']").val()};
+        batch_data[natureConstrainBound] = { ncMIN: $("input[name='ncMIN']").val(), ncMAX: $("input[name='ncMAX']").val()};
+
         e.preventDefault();
         console.log(batch_data.state," ",batch_data.numBatch)
         $.ajax({
