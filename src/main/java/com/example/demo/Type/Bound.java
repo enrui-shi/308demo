164b45b8887b377dbb4cb87d584f83cc21ae93d1
stone <enrui.shi@stonybrook.edu>
2019-04-28 22:50:11 -0400

    bound -> entity

--- src/main/java/com/example/demo/Type/Bound.java
+++ src/main/java/com/example/demo/Type/Bound.java
@@ -1,6 +1,13 @@
 package com.example.demo.Type;
 
+import javax.persistence.*;
+
+@Entity
 public class Bound {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
 
     private double upperBound;
 
