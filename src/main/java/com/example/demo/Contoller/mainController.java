0e3ff7e43d506434ed33d87a6d8e3e7c668714b4
ruiqi <ruiqi@xiamu.local>
2019-04-29 16:12:19 -0400

    state class

--- src/main/java/com/example/demo/Contoller/mainController.java
+++ src/main/java/com/example/demo/Contoller/mainController.java
@@ -1,6 +1,8 @@
 package com.example.demo.Contoller;
 
 import com.example.demo.repository.StateRepository;
+
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/home")
 public class mainController {
 
-    StateRepository stateRepository;
-    //@PostMapping(value = "/main",produces = "application/json")
+    /*StateRepository stateRepository;
+
+    @PostMapping(value = "/main", consumes = "application/json", produces = "application/json")
+    public State createState(@RequestBody State state){
+        return stateRepository.save(state);
+    }
+
+    @PostMapping(value = "/batch", consumes = "application/json", produces = "application/json")
+    public List<State> createState(@RequestBody State state){
+
+        stateRepository.save(state);
+    }*/
 }
--- src/main/java/com/example/demo/Entity/State.java
+++ src/main/java/com/example/demo/Entity/State.java
@@ -9,7 +9,7 @@ import java.util.Set;
 @Entity
 public class State {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long stateId;
 
     @OneToMany
@@ -23,7 +23,54 @@ public class State {
     @Enumerated(EnumType.STRING)
     private StateName stateName;
 
+
+
+    public State(Set<District> districts, Set<Precinct> precincts, Preference preference, StateName stateName) {
+        this.districts = districts;
+        this.precincts = precincts;
+        this.preference = preference;
+        this.stateName = stateName;
+    }
+
+    public Long getStateId() {
+        return stateId;
+    }
+
+    public Set<District> getDistricts() {
+        return districts;
+    }
+
+    public Set<Precinct> getPrecincts() {
+        return precincts;
+    }
+
+    public StateName getStateName() {
+        return stateName;
+    }
+
     public Preference getPreference() {
         return preference;
     }
+
+
+    public void setStateId(Long stateId) {
+        this.stateId = stateId;
+    }
+
+    public void setDistricts(Set<District> districts) {
+        this.districts = districts;
+    }
+
+    public void setPrecincts(Set<Precinct> precincts) {
+        this.precincts = precincts;
+    }
+
+    public void setPreference(Preference preference) {
+        this.preference = preference;
+    }
+
+    public void setStateName(StateName stateName) {
+        this.stateName = stateName;
+    }
+
 }
