3af8cd446efb5e80348fe186186d384ef1051e75
stone <enrui.shi@stonybrook.edu>
2019-04-30 16:01:04 -0400

    correct

--- src/main/java/com/example/demo/Contoller/PageController.java
+++ src/main/java/com/example/demo/Contoller/PageController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class testController {
+public class PageController {
     @RequestMapping("/")
     public String index() {
         return "login";
