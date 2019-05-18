d83664c467beb1c3340f30800681df10a227b186
Ruojin Zhang <ruojin030@gmail.com>
2019-05-18 19:25:27 -0400

    for pull

--- src/main/java/com/example/demo/Type/ClusterPair.java
+++ src/main/java/com/example/demo/Type/ClusterPair.java
@@ -27,15 +27,21 @@ public class ClusterPair {
     public Cluster combine() {
         List<ClusterEdge> edges2 = cluster2.getClusterEdges();
         for (ClusterEdge ce2 : edges2) {
+
             Cluster connect = ce2.updateCluster(cluster2, cluster1);
             if (cluster1.getNeighborClusters().contains(connect)) {
                 ClusterEdge ce1 = cluster1.getEdgeByCluster(connect);
+                connect.getNeighborClusters().remove(cluster2);
+                connect.getClusterEdges().remove(ce2);
                 ce1.merge(ce2);
             } else if (connect != cluster1) {
                 cluster1.getClusterEdges().add(ce2);
                 connect.updateNeighbour(cluster2,cluster1);
+            }else{
+                cluster1.getClusterEdges().remove(ce2);
             }
         }
+        cluster2.getNeighborClusters().remove(cluster1);
         cluster1.merge(cluster2);
         cluster1.setPaired(false);
         return cluster1;
