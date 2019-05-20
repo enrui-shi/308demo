fc9d06abc7072ebc691d332774f20d14831813dc
ruiqi <ruiqi.zhu@stonybrook.edu>
2019-05-20 07:14:13 -0400

    Update batch.js

--- src/main/resources/static/script/batch.js
+++ src/main/resources/static/script/batch.js
@@ -7,31 +7,31 @@ $(document).ready(function(){
         document.getElementById('eqerr').style.display="none";
         document.getElementById('cerr').style.display="none";
         document.getElementById('pferr').style.display="none";
-        document.getElementById('ncerr').style.display="none";
+        document.getElementById('lwerr').style.display="none";
 
-        if($("#dMAX").val() < $("#dMIN").val()){
+        if(parseInt($("#dMAX").val()) < parseInt($("#dMIN").val())){
               document.getElementById('derr').style.display="block";
               document.getElementById('derr').innerHTML="min number of district > max!";
               e.preventDefault();
-        } else if($("#mmMAX").val() < $("#mmMIN").val()){
+        } else if(parseInt($("#mmMAX").val()) < parseInt($("#mmMIN").val())){
               document.getElementById('mmerr').style.display="block";
               document.getElementById('mmerr').innerHTML="min number of majority minority district > max!";
               e.preventDefault();
-        }else if($("#eqMAX").val() < $("#eqMIN").val()){
+        }else if(parseInt($("#eqMAX").val()) < parseInt($("#eqMIN").val())){
               document.getElementById('eqerr').style.display="block";
               document.getElementById('eqerr').innerHTML="min weight of equal population > max!";
               e.preventDefault();
-        }else if($("#cMAX").val() < $("#cMIN").val()){
+        }else if(parseInt($("#cMAX").val()) < parseInt($("#cMIN").val())){
               document.getElementById('cerr').style.display="block";
               document.getElementById('cerr').innerHTML="min weight of compactness > max!";
               e.preventDefault();
-        }else if($("#pfMAX").val() < $("#pfMIN").val()){
-            document.getElementById('pferr').style.display="block";
+        }else if(parseInt($("#pfMAX").val()) < parseInt($("#pfMIN").val())){
+              document.getElementById('pferr').style.display="block";
               document.getElementById('pferr').innerHTML="min weight of partisan fairness > max!";
               e.preventDefault();
-        }else if($("#ncMAX").val() < $("#ncMIN").val()){
-              document.getElementById('ncerr').style.display="block";
-              document.getElementById('ncerr').innerHTML="min weight of natural constraint > max!";
+        }else if(parseInt($("#lwMAX").val()) < parseInt($("#lwMIN").val())){
+              document.getElementById('lwerr').style.display="block";
+              document.getElementById('lwerr').innerHTML="min weight of length width compactness > max!";
               e.preventDefault();
         } else {
 
@@ -40,29 +40,18 @@ $(document).ready(function(){
               var ep_data = {upperBound: $("#eqMAX").val(), lowerBound: $("#eqMIN").val()};
               var c_data = {upperBound: $("#cMAX").val(), lowerBound: $("#cMIN").val()};
               var pf_data = {upperBound: $("#pfMAX").val(), lowerBound: $("#pfMIN").val()};
-              var nc_data = {upperBound: $("#ncMAX").val(), lowerBound: $("#ncMIN").val()};
+              var lw_data = {upperBound: $("#lwMAX").val(), lowerBound: $("#lwMIN").val()};
 
               var batch_data = {numBatch: $('#num-of-batch').val(), numDistrictBound: d_data, stateName:$("input[name='state']:checked").val(),
                   numOfMMBound: mm_data, equalPopulationBound: ep_data, compactnessBound: c_data,
-                  partisanFairnessBound: pf_data, natureConstrainBound: nc_data};
+                  partisanFairnessBound: pf_data, lengthWidthCompactnessBound: lw_data};
 
               e.preventDefault();
               console.log(batch_data)
 
               document.getElementById('batchrun').style.display='block';
 
-              /*var elem = document.getElementById("myBar");
-              var width = 10;
 
-              function frame() {
-                  if (width >= 100) {
-                      clearInterval(id);
-                  } else {
-                      width++;
-                      elem.style.width = width + '%';
-                      elem.innerHTML = width * 1  + '%';
-                  }
-              }*/
 
               console.log("start to batch");
               $.ajax({
@@ -77,6 +66,20 @@ $(document).ready(function(){
                       console.log("batch "+data);
                   }
               })
+              var elem = document.getElementById("myBar");
+              var width = 1;
+              var id = setInterval(frame, 100);
+
+              function frame() {
+                  if (width >= 100) {
+                    clearInterval(id);
+                  } else {
+                    width++;
+                    elem.style.width = width + '%';
+                    elem.innerHTML = width * 1  + '%';
+                  }
+              }
+
         }
     })
 });
