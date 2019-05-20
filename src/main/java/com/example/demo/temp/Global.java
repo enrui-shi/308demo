163c556d3625a032cb5e5eac435b7ff5077cbecd
Ruojin Zhang <ruojin030@gmail.com>
2019-05-20 00:42:24 -0400

    for pull

--- src/main/java/com/example/demo/temp/Global.java
+++ src/main/java/com/example/demo/temp/Global.java
@@ -12,7 +12,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-//@Component
+@Component
 public class Global implements CommandLineRunner {
 
     @Autowired
@@ -20,54 +20,54 @@ public class Global implements CommandLineRunner {
 
     public void run(String args[]){
         System.out.println("start init precinct");
-        List<Precinct> ohP = initService.getPrecinctsByState(StateName.OH);
+        //List<Precinct> ohP = initService.getPrecinctsByState(StateName.OH);
         List<Precinct> iaP = initService.getPrecinctsByState(StateName.IA);
-        List<Precinct> nyP = initService.getPrecinctsByState(StateName.NY);
+        //List<Precinct> nyP = initService.getPrecinctsByState(StateName.NY);
         System.out.println("finish");
-        System.out.println("OH precinct size:"+ohP.size());
+        //System.out.println("OH precinct size:"+ohP.size());
         System.out.println("IA precinct size:"+iaP.size());
-        System.out.println("NY precinct size:"+nyP.size());
+        //System.out.println("NY precinct size:"+nyP.size());
 
         System.out.println("start init edge");
-        List<PrecinctEdge> ohE = initService.getPrecinctEdgeByState(StateName.OH);
+        //List<PrecinctEdge> ohE = initService.getPrecinctEdgeByState(StateName.OH);
         List<PrecinctEdge> iaE = initService.getPrecinctEdgeByState(StateName.IA);
-        List<PrecinctEdge> nyE = initService.getPrecinctEdgeByState(StateName.NY);
+        //List<PrecinctEdge> nyE = initService.getPrecinctEdgeByState(StateName.NY);
         System.out.println("finish");
-        System.out.println("OH edge size: "+ohE.size());
+        //System.out.println("OH edge size: "+ohE.size());
         System.out.println("IA edge size: "+iaE.size());
-        System.out.println("NY edge size: "+nyE.size());
+        //System.out.println("NY edge size: "+nyE.size());
 
         System.out.println("Starting convert precinct to cluster");
-        Map<Long,Cluster> oh_map= addData(ohP);
+        //Map<Long,Cluster> oh_map= addData(ohP);
         Map<Long,Cluster> ia_map= addData(iaP);
-        Map<Long,Cluster> ny_map= addData(nyP);
+        //Map<Long,Cluster> ny_map= addData(nyP);
         // add precinct ot global
-        GVAL.oh = new ArrayList<>(oh_map.values());
+        //GVAL.oh = new ArrayList<>(oh_map.values());
         GVAL.ia = new ArrayList<>(ia_map.values());
-        GVAL.ny = new ArrayList<>(ny_map.values());
+        //GVAL.ny = new ArrayList<>(ny_map.values());
         System.out.println("finish convert precinct");
         System.out.println("start convert edge");
-        GVAL.ohe = addEdge(ohE,oh_map);
+        //GVAL.ohe = addEdge(ohE,oh_map);
         GVAL.iae = addEdge(iaE,ia_map);
-        GVAL.nye = addEdge(nyE,ny_map);
+        //GVAL.nye = addEdge(nyE,ny_map);
 
         System.out.println("finish convert edge");
 
 
         //check
-        System.out.println("OH: "+ GVAL.oh.size());
-        System.out.println("OH edge: "+ GVAL.ohe.size());
+        //System.out.println("OH: "+ GVAL.oh.size());
+        //System.out.println("OH edge: "+ GVAL.ohe.size());
         System.out.println("IA: "+ GVAL.ia.size());
         System.out.println("IA edge: "+ GVAL.iae.size());
-        System.out.println("NY: " + GVAL.ny.size());
-        System.out.println("NY edge: " + GVAL.nye.size());
+        //System.out.println("NY: " + GVAL.ny.size());
+        //System.out.println("NY edge: " + GVAL.nye.size());
 
-        System.out.println(GVAL.oh.get(0));
-        System.out.println(GVAL.ohe.get(0));
+        //System.out.println(GVAL.oh.get(0));
+        //System.out.println(GVAL.ohe.get(0));
         System.out.println(GVAL.ia.get(0));
         System.out.println(GVAL.iae.get(0));
-        System.out.println(GVAL.ny.get(0));
-        System.out.println(GVAL.nye.get(0));
+        //System.out.println(GVAL.ny.get(0));
+        //System.out.println(GVAL.nye.get(0));
     }
     public Map<Long,Cluster> addData(List<Precinct> lp){
         Map<Long,Cluster> map = new HashMap<>();
